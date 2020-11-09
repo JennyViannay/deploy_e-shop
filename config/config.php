0f16d5bf993fb17b41dd74b96bf9921a150db21e
JennyViannay <jenny.viannay75@gmail.com>
2020-11-09 20:59:41 +0100

    try something for debug deploy

--- config/config.php
+++ config/config.php
@@ -2,8 +2,8 @@
 
 
 define('APP_DEV', true);
-define('APP_VIEW_PATH', __DIR__ . '/../src/View/');
-define('APP_CACHE_PATH', __DIR__ . '/../temp/cache/');
+define('APP_VIEW_PATH', __DIR__ . '../src/View/');
+define('APP_CACHE_PATH', __DIR__ . '../temp/cache/');
 
 define('HOME_PAGE', 'home/index');
 
