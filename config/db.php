19ab0bc84ba8949daa4b8cd953c302fadc48197f
JennyViannay <jenny.viannay75@gmail.com>
2020-11-09 23:34:47 +0100

    change config for dev ENV

--- config/db.php
+++ config/db.php
@@ -1,11 +1,11 @@
 <?php
 
-define('APP_DB_USER', 'e_shop_7681');
-define('APP_DB_PWD', 'UpEtKj_btSaNJrF8h96S');
-define('APP_DB_HOST', 'a50664d0-7221-4cf2-806f-a2b303ed6ffc.e-shop-7681.mysql.dbs.scalingo.com:33820/e_shop_7681?useSSL=true&verifyServerCertificate=false');
-define('APP_DB_NAME', 'e_shop_7681');
+// define('APP_DB_USER', 'e_shop_7681');
+// define('APP_DB_PWD', 'UpEtKj_btSaNJrF8h96S');
+// define('APP_DB_HOST', 'a50664d0-7221-4cf2-806f-a2b303ed6ffc.e-shop-7681.mysql.dbs.scalingo.com:33820/e_shop_7681?useSSL=true&verifyServerCertificate=false');
+// define('APP_DB_NAME', 'e_shop_7681');
 
-// define('APP_DB_USER', 'root');
-// define('APP_DB_PWD', 'root');
-// define('APP_DB_HOST', 'localhost:8889');
-// define('APP_DB_NAME', 'livecoding-pdo');
+define('APP_DB_USER', 'root');
+define('APP_DB_PWD', 'root');
+define('APP_DB_HOST', 'localhost:8889');
+define('APP_DB_NAME', 'livecoding-pdo');
