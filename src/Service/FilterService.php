c3722fe888b8223ddb0896e6b408502aff1a7f39
JennyViannay <jenny.viannay75@gmail.com>
2021-04-15 22:56:54 +0200

    fix filter

--- src/Service/FilterService.php
+++ src/Service/FilterService.php
@@ -8,6 +8,18 @@ class FilterService
 {
     public function getArticlesFromSearch(array $search){
         $articleManager = new ArticleManager();
+        if (!empty($search['brand_id']) && !empty($search['size_id']) && !empty($search['color_id'])) {
+            return $articleManager->searchFull($search['color_id'], $search['size_id'], $search['brand_id']);
+        }
+        if (!empty($search['size_id']) && !empty($search['color_id'])) {
+            return $articleManager->searchBySizeAndColor($search['size_id'], $search['color_id']);
+        }
+        if (!empty($search['size_id']) && !empty($search['brand_id'])) {
+            return $articleManager->searchBySizeAndBrand($search['size_id'], $search['brand_id']);
+        }
+        if (!empty($search['color_id']) && !empty($search['brand_id'])) {
+            return $articleManager->searchByColorAndBrand($search['color_id'], $search['brand_id']);
+        }
         if (!empty($search['brand_id'])) {
             return $articleManager->searchByBrand($search['brand_id']);
         }
@@ -16,18 +28,6 @@ class FilterService
         }
         if (!empty($search['size_id'])) {
             return $articleManager->searchBySize($search['size_id']);
-        }
-        if (!empty($search['color_id']) && !empty($search['brand_id'])) {
-            return $articleManager->searchByColorAndBrand($search['color_id'], $search['brand_id']);
-        }
-        if (!empty($search['size_id']) && !empty($search['brand_id'])) {
-            return $articleManager->searchBySizeAndBrand($search['size_id'], $search['brand_id']);
-        }
-        if (!empty($search['size_id']) && !empty($search['color_id'])) {
-            return $articleManager->searchBySizeAndColor($search['size_id'], $search['color_id']);
-        }
-        if (!empty($search['brand_id']) && !empty($search['size_id']) && !empty($search['color_id'])) {
-            return $articleManager->searchFull($search['color_id'], $search['size_id'], $search['brand_id']);
         }
     }
 }
